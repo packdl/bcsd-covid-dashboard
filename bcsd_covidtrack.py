7ae2adc4dd75b96b9a6ca6c37d19efaacf725452
Derik <dpack@ieee.org>
2021-10-11 11:58:00 -0400

    Refactoring to increase readability

--- bcsd_covidtrack.py
+++ bcsd_covidtrack.py
@@ -5,8 +5,6 @@ from playwright.sync_api import Playwright, sync_playwright
 
 from schools import get_schools, parseschooldata, gettableheader
 
-elem = {"Mt Holly Elementary":"MHE","Mount Holly Elementary": "MHE", "Berkeley County School District": "BCSD"}
-
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch()
     context = browser.new_context()
@@ -14,15 +12,15 @@ def run(playwright: Playwright) -> None:
     # Open new page
     page = context.new_page()
 
-    # Go to https://www.bcsdschools.net/Domain/8307
+    # Go to COIVD Dashboard at https://www.bcsdschools.net/Domain/8307
     page.goto("https://www.bcsdschools.net/Domain/8307")
     
+    # Get list of schools from the drop down in the page
     our_element = page.query_selector("#pmi-43630")
     school_data = our_element.inner_html()
-
-    # Get list of schools from the drop down in the page
     schools = get_schools(school_data)
 
+    # Get COVID data for each school
     rows = []
     for school in schools:
         print(school)
@@ -30,18 +28,18 @@ def run(playwright: Playwright) -> None:
 
         page.click('input:has-text("Search")')
         page.wait_for_timeout(1000)
-
-        # Save header if school is Berkeley County School District
+        
         element_handle = page.query_selector("#pmi-43630")
-
-        # Get html of target element and parse out and store data
-        div = element_handle.inner_html()
+        
+        # Save header if school is Berkeley County School District
         if 'Berkeley County School District'.lower() in school.lower():
-            header = gettableheader(element_handle.inner_html())
-            with open("output/header.csv","a", newline="") as headerfile:
-                csv.writer(headerfile).writerow((datetime.now().date(),) + header)
+            retrieve_header(element_handle)
+
+        # Get COVID data for school and store in list
+        div = element_handle.inner_html()
         rows.append(parseschooldata(div))
 
+    # Write data for schools to text file
     with open("output/data.csv", "a", newline="") as fp:
         cvswriter = csv.writer(fp)
         cvswriter.writerows(rows)
@@ -49,6 +47,11 @@ def run(playwright: Playwright) -> None:
     # ---------------------
     context.close()
     browser.close()
+
+def retrieve_header(element_handle):
+    header = gettableheader(element_handle.inner_html())
+    with open("output/header.csv","a", newline="") as headerfile:
+        csv.writer(headerfile).writerow((datetime.now().date(),) + header)
 
 with sync_playwright() as playwright:
     run(playwright)
